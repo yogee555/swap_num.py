a1301c6e026a69c74ea0d940bf909af5821518c4
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-23 21:25:41 +0530

    Add files via upload

---
+++ swap_num.py
@@ -0,0 +1,6 @@
+# Swaps numbers in python
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print(f"Before swapping: num1 = {num1}, num2 = {num2}")
+num1,num2=num2,num1
+print(f"Enter the number:num1={num1},num2={num2}")
